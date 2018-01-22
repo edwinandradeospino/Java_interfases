c80a15562c44f10a210f1c5c53dc0731f7165555
EdwinAndradeOspino <edwin230616@gmail.com>
2018-01-22 16:49:47 -0500

    game 02

--- CapitalGame/src/com/edwsoft/capitalGame/GUIGame.java
+++ CapitalGame/src/com/edwsoft/capitalGame/GUIGame.java
@@ -17,8 +17,8 @@ public class GUIGame extends GameMath {
 		} catch (Exception e) {
 		
 		}
-		
-		
 	}
-	
+	public void methodeFils() {
+		System.out.println("estoy en la clase hija");
+	}
 }
--- CapitalGame/src/com/edwsoft/capitalGame/Testeur.java
+++ CapitalGame/src/com/edwsoft/capitalGame/Testeur.java
@@ -4,6 +4,10 @@ public class Testeur {
 	public static void main(String[] args) {
 		//(NombreJeux, Nombre de Chiffre, Operations)
 		new GUIGame(2, 1, '-').startGame();
+
+		// Utilizacion de una clase hija con (casting)
+		((GUIGame)(new GUIGame(4, 4, '+'))).methodeFils();
+		
 		
 
 	}
